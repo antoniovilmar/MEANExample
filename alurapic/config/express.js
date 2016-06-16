87c10f337045aabfae166327c8899f9bb36a68f6
Antonio Castro <antoniovilmar.castro@gmail.com>
2016-06-16 00:28:14 -0300

    Usando consign para integração API e Rota

--- alurapic/config/express.js
+++ alurapic/config/express.js
@@ -4,6 +4,9 @@
 
   app.use(express.static('./public'));
 
-  consign().include('app/routes').into(app);
+  consign({cwd: 'app' })
+    .include('api')
+    .then('routes')
+    .into(app);
 
   module.exports = app;
