f43613befb7e92214220d42016a7f97f32f19ea6
Antonio Castro <antoniovilmar.castro@gmail.com>
2016-06-19 21:45:08 -0300

    Criando rota para busca por Id da foto

--- alurapic/app/api/foto.js
+++ alurapic/app/api/foto.js
@@ -1,16 +1,25 @@
 var api = {};
+var fotos = [{
+    _id: 1,
+    titulo: 'Leão',
+    url: 'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg?fit=2560%2C1600'
+}, {
+    _id: 2,
+    titulo: 'Leão2',
+    url: 'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg?fit=2560%2C1600'
+}];
 
 api.lista = function(req, res) {
-    var fotos = [{
-        _id: 1,
-        titulo: 'Leão',
-        url: 'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg?fit=2560%2C1600'
-    }, {
-        _id: 2,
-        titulo: 'Leão2',
-        url: 'http://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg?fit=2560%2C1600'
-    }];
+
     res.json(fotos);
 }
 
-module.exports=api;
+api.buscaPorId = function(req, res) {
+    var foto = fotos.find(function(foto) {
+        return foto._id == req.params.id;
+    });
+
+    res.json(foto);
+}
+
+module.exports = api;
