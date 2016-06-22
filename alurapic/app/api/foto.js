f4e453119889c5837adaf951f4195f0806f5b87a
Antonio Castro <antoniovilmar.castro@gmail.com>
2016-06-22 02:12:08 -0300

    Integrando API de busca por id, adicionar, remover e atualizar com mongoose

--- alurapic/app/api/foto.js
+++ alurapic/app/api/foto.js
@@ -1,32 +1,62 @@
 var mongoose = require('mongoose');
 
 var api = {};
-
+var model = mongoose.model('Foto');
 
 api.lista = function(req, res) {
-    var model = mongoose.model('Foto');
-    model
-    .find({})
-    .then(function(fotos){
-        res.json(fotos);
-    }, function(error){
-        console.log(error);
-        res.sendStatus(500).json(error);
-    });
-
+    model.find({})
+        .then(function(fotos) {
+            res.json(fotos);
+        }, function(error) {
+            console.log(error);
+            res.sendStatus(500).json(error);
+        });
 }
 
 api.buscaPorId = function(req, res) {
+    model.findById(req.params.id)
+        .then(function(foto) {
+            if (!foto) throw new Error('Foto não encontrada');
+            res.json(foto);
+        }, function(error) {
+            console.log(error);
+            res.sendStatus(404).json(error);
+        });
 }
 
 api.removePorId = function(req, res) {
+    model.remove({
+            '_id': req.params.id
+        })
+        .then(function() {
+            res.sendStatus(200);
+        }, function(error) {
+            console.log(error);
+            res.sendStatus(500).json(error);
+        });
 }
 
 api.adiciona = function(req, res) {
 
+    model.create(req.body)
+        .then(function(foto) {
+            res.json(foto);
+        }, function(error) {
+            console.log(error);
+            res.sendStatus(500).json(error);
+        });
+
 }
 
-api.atualiza = function(req, res){
+api.atualiza = function(req, res) {
+
+    model.findByIdAndUpdate(req.params.id, req.body)
+        .then(function(foto) {
+            res.json(foto);
+        }, function(error) {
+            console.log(error);
+            res.sendStatus(500).json(error);
+        });
 
 
 };
