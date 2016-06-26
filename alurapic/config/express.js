fb947ecd66b5b2087c4ab7152511db25eefcc119
Antonio Castro <antoniovilmar.castro@gmail.com>
2016-06-26 13:28:33 -0300

    Autenticação da aplicação via token

--- alurapic/config/express.js
+++ alurapic/config/express.js
@@ -2,12 +2,13 @@
   var app = express();
   var consign = require('consign');
   var bodyParser = require('body-parser');
-
+  app.set('secret', 'tokenalurapic')
   app.use(express.static('./public'));
   app.use(bodyParser.json());
   consign({cwd: 'app' })
     .include('models')
     .then('api')
+    .then('routes/auth.js')
     .then('routes')
     .into(app);
 
--- alurapic/public/js/main.js
+++ alurapic/public/js/main.js
@@ -1,19 +1,22 @@
 angular.module('alurapic', ['minhasDiretivas','ngAnimate', 'ngRoute', 'ngResource', 'meusServicos'])
-	.config(function($routeProvider, $locationProvider) {
+	.config(function($routeProvider, $locationProvider, $httpProvider) {
 
+		$httpProvider.interceptors.push('tokenInterceptor');
 		$routeProvider.when('/fotos', {
 			templateUrl: 'partials/principal.html',
 			controller: 'FotosController'
-		});
-
-		$routeProvider.when('/fotos/new', {
+		})
+		.when('/fotos/new', {
 			templateUrl: 'partials/foto.html',
 			controller: 'FotoController'
-		});
-
-		$routeProvider.when('/fotos/edit/:fotoId', {
+		})
+		.when('/fotos/edit/:fotoId', {
 			templateUrl: 'partials/foto.html',
 			controller: 'FotoController'
+		})
+		.when('/login',{
+			templateUrl: 'partials/login.html',
+			controller: 'LoginController'
 		});
 
 		$routeProvider.otherwise({redirectTo: '/fotos'});
